5242c0b314c51811a121792656746e64b735d5d0
Elia G <54690022+eliakaicode@users.noreply.github.com>
2019-12-19 12:14:22 -0500

    my javascript file

---
+++ finale.js
@@ -0,0 +1,39 @@
+
+
+
+
+function toggleText1() {
+  var text = document.getElementById("demo1");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function toggleText2() {
+  var text = document.getElementById("demo2");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function toggleText3() {
+  var text = document.getElementById("demo3");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function toggleText4() {
+  var text = document.getElementById("demo4");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
